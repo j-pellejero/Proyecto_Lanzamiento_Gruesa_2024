922904e8c05d3d7100a59f0eea823b4e4e9a882c
j-pellejero <jpellejero@bc.org.ar>
2024-08-26 11:25:11 -0300

    actualizo lista

--- Demo_Git_GitHub/Lista.py
+++ Demo_Git_GitHub/Lista.py
@@ -1,5 +1,5 @@
 #Encuentro 3 de Git a GitHub
 
 #Crear una lista para ver los cambios entre usuarios
-lista_cambios = ["sorgo", "soja", "maiz", "cebada", "trigo", "girasol", "algodon", "carinata", "avena", "vicia", "poroto", "tincho"]
+lista_cambios = ["sorgo", "soja", "maiz", "cebada", "trigo", "girasol", "algodon", "carinata", "avena", "vicia", "poroto", "tincho", "colza"]
 print (lista_cambios)
