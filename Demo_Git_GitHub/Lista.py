83f43f3d69ee468f137ec82ba66bb6cbf46bef13
Dani Ventu <dventurinobc@gmail.com>
2024-08-26 09:49:11 -0300

    Update Lista.py
    Agregado de cultivos en la lista

--- Demo_Git_GitHub/Lista.py
+++ Demo_Git_GitHub/Lista.py
@@ -2,5 +2,5 @@
 
 #Crear una lista para ver los cambios entre usuarios
 
-lista_cambios = ["sorgo", "soja", "maiz", "cebada"]
+lista_cambios = ["sorgo", "soja", "maiz", "cebada", "trigo", "girasol", "algodon", "carinata", "avena",]
 print (lista_cambios)
